cc93d49948addc0ff5f35b47578fe9794bf02600
NJ-NafsinJahan <nj.nafsinjahan@gmail.com>
2026-06-26 23:56:46 +0600

    post for writercollecton

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -9,6 +10,61 @@ const port = process.env.PORT;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    //! ***** database create
+    const db = client.db("fableDB");
+    const writerCollection = client.db("writers");
+    const ebooksCollection = client.db("ebooks");
+    const bookingCollection = client.db("bookings");
+    const paymentCollection = client.db("payments");
+    const bookmarkCollection = client.db("bookmarks");
+
+    // ********
+    // POST API for writer-profile
+    app.post("/api/writer-profile", async (req, res) => {
+      const { writerName, image, website, bio, writerEmail } = req.body;
+
+      const addData = {
+        writerName,
+        image,
+        website,
+        bio,
+        writerEmail,
+        createdAt: new Date(),
+        status: "active",
+      };
+
+      const result = await writerCollection.insertOne(addData);
+      res.json(result);
+    });
+
+    // ! ******    *******      ******
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
